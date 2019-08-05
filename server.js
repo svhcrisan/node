c79452319be9b1ce5a0449b1c125614bc315b898
Horea Crisan <horea.crisan@softvision.ro>
2019-08-05 17:56:24 +0300

    updates

--- server.js
+++ server.js
@@ -1,19 +1,73 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    fs.readFile("./Data/products.json", (err, data) => { res.end(data.toString()) });
-    console.log(res);
+// declare paths
+const categoriesPath = /(^[\/]categories([\/]?)([a-z0-9])?$)/;
+const productsPath = /(^[\/]products[\/]?$)/;
+const idPath = /^[0-9]*$/;
 
-    if (req.url === "/categories") {
-        fs.readFile("./Data/categories.json", (err, data) => { res.end(data.toString()) });
+http.createServer((req, res) => {
+    const url = req.url;
+
+    console.log(url);
+
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    if (productsPath.test(url)) {
+        fs.readFile("./Data/products.json", (err, data) => {
+            try {
+
+
+                /*
+                for (const i = 0; i < objArr.length; i++) {
+                    if (idPath.test(url)) {
+                        res.end(objArr[i].toString());
+                    }
+                }
+                */
+
+
+
+
+                //console.log(objArr[0]);
+                res.end(data.toString());
+            } catch (err) {
+                return err;
+            }
+        });
+    }
+    if (categoriesPath.test(url)) {
+        fs.readFile("./Data/categories.json", (err, data) => {
+            try {
+                res.end(data.toString())
+            } catch (err) {
+                return err;
+            }
+        });
+
     }
 
 
-
-
-
+    if (idPath.test(categoriesPath)) {
+        console.log("intra");
+        /*
+                fs.readFile("./Data/products.json", (err, data) => {
+        
+                    const objArr = JSON.parse(data);
+                    const pId = (url.match(idPath) || []).slice(-1);
+        
+                    //console.log(pId);
+                    if (pId) {
+                        console.log(product.id);
+                        const found = objArr.filter(product = product.id === pId);
+                        res.end(found);
+                        return;
+                    }
+                }
+                */
+    }
 
 })
     .listen(5000, "127.0.0.1");
