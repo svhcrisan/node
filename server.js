91b31c200a8655269ab267a07808e64b5b5f8a2b
Horea Crisan <horea.crisan@softvision.ro>
2019-08-06 16:59:10 +0300

    update day1 express

--- server.js
+++ server.js
@@ -2,38 +2,12 @@ const express = require('express');
 const http = require('http');
 const router = express.Router();
 const app = express();
-
 const categories = require("./Data/categories.json");
 const products = require("./Data/products.json");
-
-//console.log(categories)
-
-// show products by categories
-for (let i = 0; i < categories.length; i++) {
-    router.get(`/categories/${i}/products`, (req, res) => {
-        const productsByIndex = products.map((elem, index) => {
-            //let productId = elem.product_id;
-            //console.log(categories[i].id);
-            if (elem.product_categories.includes(categories[i].id)) {
-                return elem;
-            }
-        });
-        const checkNull = (product) => {
-            return product != null;
-        }
-        //console.log(products)
-        res.status(200).json(productsByIndex.filter(checkNull));
-    });
-}
 
 // show categories
 router.get('/categories', (req, res) => {
     res.status(200).json(categories);
-});
-
-// show products
-router.get('/products', (req, res) => {
-    res.status(200).json(products);
 });
 
 // show categories by id
@@ -43,11 +17,38 @@ router.get('/categories/:id', (req, res) => {
         const oneCategory = res.status(200).json(categories[categoryId]);
         return oneCategory;
     }
-    return res.status(404).json({ errors: "Category not found!" });
+    return res.status(404).json({ errors: [{ message: "Category not found." }] });
 });
 
+// show products by categories
+for (let i = 0; i < categories.length; i++) {
+    router.get(`/categories/${i}/products`, (req, res) => {
+        const productsByIndex = products.map((elem, index) => {
+            if (elem.product_categories.includes(categories[i].id)) {
+                return elem;
+            }
+        });
+        const checkNull = (product) => {
+            return product != null;
+        }
+        res.status(200).json(productsByIndex.filter(checkNull));
+    });
+}
 
+// show products
+router.get('/products', (req, res) => {
+    res.status(200).json(products);
+});
 
+// show products by id
+router.get('/products/:id', (req, res) => {
+    const productId = req.params.id;
+    if (products[productId]) {
+        const oneProduct = res.status(200).json(products[productId]);
+        return oneProduct;
+    }
+    return res.status(404).json({ errors: [{ message: "Product not found." }] });
+});
 
 app.use('/', router);
 //declare port where we want to connect
